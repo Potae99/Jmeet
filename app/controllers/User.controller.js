9569ad57c822d25c0568a889ca09c716195df949
Potae99 <101413178+Potae99@users.noreply.github.com>
2024-04-18 17:15:44 +0700

    add req print UserID from token

--- app/controllers/User.controller.js
+++ app/controllers/User.controller.js
@@ -2,7 +2,7 @@ exports.allAccess = (req, res) => {
     res.status(200).send("Public Content.");
 };
 exports.userBoard = (req, res) => {
-    console.log(req);
+    console.log("UserID is = "+ req.UserID);////print from token
     res.status(200).send("User Content.");
 };
 exports.adminBoard = (req, res) => {
